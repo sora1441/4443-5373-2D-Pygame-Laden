2cd9d01212e82761a3607a33e40f5cb5453959ac
sora1441 <43249102+sora1441@users.noreply.github.com>
2020-08-09 19:41:13 -0500

    Add files via upload

---
+++ Assignments/P02/main.py
@@ -0,0 +1,244 @@
+
+# KidsCanCode - Game Development with Pygame video series
+# Jumpy! (a platform game) - Part 18
+# Video link: https://youtu.be/i0PaigPo6KM
+# scrolling cloud background
+# Art from Kenney.nl
+# Happy Tune by http://opengameart.org/users/syncopika
+# Yippee by http://opengameart.org/users/snabisch
+
+
+import pygame as pygame
+import random
+from settings import *
+from sprites import *
+from os import path
+
+class Game:
+    def __init__(self):
+        # initialize game window, etc
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.font_name = pygame.font.match_font(FONT_NAME)
+        self.load_data()
+        self.needed_to_jump = 0
+        self.items_collected = 0
+        self.jump_no = 0
+
+    def load_data(self):
+        # load high score
+        self.dir = path.dirname(__file__)
+        # load spritesheet image
+        img_dir = path.join(self.dir, 'img')
+        self.spritesheet = Spritesheet(path.join(img_dir, SPRITESHEET))
+        # cloud images
+        self.cloud_images = []
+        for i in range(1, 4):
+            self.cloud_images.append(pygame.image.load(path.join(img_dir, 'cloud{}.png'.format(i))).convert())
+        # load sounds
+        self.snd_dir = path.join(self.dir, 'snd')
+        self.jump_sound = pygame.mixer.Sound(path.join(self.snd_dir, 'Jump33.wav'))
+        self.boost_sound = pygame.mixer.Sound(path.join(self.snd_dir, 'Boost16.wav'))
+
+    def new(self):
+        # start a new game
+        self.score = 0
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.platforms = pygame.sprite.Group()
+        self.powerups = pygame.sprite.Group()
+        self.mobs = pygame.sprite.Group()
+        self.clouds = pygame.sprite.Group()
+        self.player = Player(self)
+        for plat in PLATFORM_LIST:
+            Platform(self, *plat)
+        self.mob_timer = 0
+        pygame.mixer.music.load(path.join(self.snd_dir, 'Happy Tune.ogg'))
+        for i in range(8):
+            c = Cloud(self)
+            c.rect.y += 500
+        self.run()
+
+    def run(self):
+        # Game Loop
+        pygame.mixer.music.play(loops=-1)
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+        pygame.mixer.music.fadeout(500)
+
+    def update(self):
+        # Game Loop - Update
+        self.all_sprites.update()
+
+        # spawn a mob?
+        now = pygame.time.get_ticks()
+        if now - self.mob_timer > 5000 + random.choice([-1000, -500, 0, 500, 1000]):
+            self.mob_timer = now
+            Mob(self)
+        # hit mobs?
+        mob_hits = pygame.sprite.spritecollide(self.player, self.mobs, False, pygame.sprite.collide_mask)
+        if mob_hits:
+            self.playing = False
+
+        # check if player hits a platform - only if falling
+        if self.player.vel.y > 0:
+            hits = pygame.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                lowest = hits[0]
+                for hit in hits:
+                    if hit.rect.bottom > lowest.rect.bottom:
+                        lowest = hit
+                if self.player.pos.x < lowest.rect.right + 10 and \
+                   self.player.pos.x > lowest.rect.left - 10:
+                    if self.player.pos.y < lowest.rect.centery:
+                        self.player.pos.y = lowest.rect.top
+                        self.player.vel.y = 0
+                        self.player.jumping = False
+
+        # if player reaches top 1/4 of screen
+        if self.player.rect.top <= HEIGHT / 4:
+            if random.randrange(100) < 15:
+                Cloud(self)
+            self.player.pos.y += max(abs(self.player.vel.y), 2)
+            for cloud in self.clouds:
+                cloud.rect.y += max(abs(self.player.vel.y / 2), 2)
+            for mob in self.mobs:
+                mob.rect.y += max(abs(self.player.vel.y), 2)
+            for plat in self.platforms:
+                plat.rect.y += max(abs(self.player.vel.y), 2)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+                    self.score += 10
+
+        # if player hits powerup
+        pow_hits = pygame.sprite.spritecollide(self.player, self.powerups, True)
+        for pow in pow_hits:
+            if pow.type == 'boost':
+                self.boost_sound.play()
+                self.needed_to_jump +=1
+                self.items_collected +=1
+                if self.needed_to_jump == 3:
+                    self.player.vel.y = -BOOST_POWER
+                    self.player.jumping = False
+                    self.needed_to_jump = 0
+                    self.jump_no +=1
+                    if self.items_collected == 10:
+                        self.playing = False
+
+
+
+        # Die!
+        if self.player.rect.bottom > HEIGHT:
+            for sprite in self.all_sprites:
+                sprite.rect.y -= max(self.player.vel.y, 10)
+                if sprite.rect.bottom < 0:
+                    sprite.kill()
+                    self.items_collected=0
+                    self.jump_no = 0
+        if len(self.platforms) == 0:
+            self.playing = False
+
+        # spawn new platforms to keep same average number
+        while len(self.platforms) < 6:
+            width = random.randrange(50, 100)
+            Platform(self, random.randrange(0, WIDTH - width),
+                     random.randrange(-75, -30))
+
+    def events(self):
+        # Game Loop - events
+        for event in pygame.event.get():
+            # check for closing window
+            if event.type == pygame.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.player.jump()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    self.player.jump_cut()
+
+    def draw(self):
+        # Game Loop - draw
+        self.screen.fill(BGCOLOR)
+        self.all_sprites.draw(self.screen)
+        self.draw_text(str(self.items_collected), 22, WHITE, WIDTH / 2, 15)
+        # *after* drawing everything, flip the display
+        pygame.display.flip()
+
+    def show_start_screen(self):
+        # game splash/start screen
+        pygame.mixer.music.load(path.join(self.snd_dir, 'Yippee.ogg'))
+        pygame.mixer.music.play(loops=-1)
+        clicked=False
+        self.screen.fill(BGCOLOR)
+        self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("Arrows to move, Space to jump", 22, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Press a key to play", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        self.draw_text("Get 3 medals to jump and 15 to win!", 22, BLACK, WIDTH / 2, 15)
+        pygame.display.flip()
+        self.wait_for_key()
+        pygame.mixer.music.fadeout(500)
+
+    def show_go_screen(self):
+        # game over/continue
+        if not self.running:
+            return
+        pygame.mixer.music.load(path.join(self.snd_dir, 'Yippee.ogg'))
+        pygame.mixer.music.play(loops=-1)
+        self.screen.fill(BGCOLOR)
+        if self.items_collected !=10:
+            self.draw_text("GAME OVER", 48, WHITE, WIDTH / 2, HEIGHT / 4)
+            self.draw_text("Score: " + str(self.items_collected), 22, WHITE, WIDTH / 2, HEIGHT / 2)
+            self.draw_text("Press a key to play again", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        if self.items_collected==10:
+            self.draw_text("Congradulations!!", 50, WHITE, WIDTH / 2, HEIGHT /4)
+            self.draw_text("Press a key to play again", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+            self.items_collected=0
+            self.jump_no =0
+        pygame.display.flip()
+        self.wait_for_key()
+        pygame.mixer.music.fadeout(500)
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    waiting = False
+                    self.running = False
+                if event.type == pygame.KEYUP:
+                    waiting = False
+
+    def draw_text(self, text, size, color, x, y):
+        font = pygame.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
+    def show_splash(self):
+        waiting = True
+        while waiting:
+            self.screen.blit("splash.png", (0,0))
+            for event in pygame.event.get():
+                if event.type == pygame.KEYUP:
+                    waiting = False
+
+g = Game()
+g.show_splash
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pygame.quit()
